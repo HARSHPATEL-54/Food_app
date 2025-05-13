da68cd7df7807d7d5bcda09a9e1be9bb8febad03
Harsh Patel cs54 <120240334+HARSHPATEL-54@users.noreply.github.com>
2025-05-13 23:44:04 +0530

    Update useUserStore.ts

--- client/src/store/useUserStore.ts
+++ client/src/store/useUserStore.ts
@@ -4,7 +4,7 @@ import axios from "axios";
 import { LoginInputState, SignupInputState } from "@/schema/userSchema";
 import { toast } from "sonner";
 
-const API_END_POINT = "{import.meta.env.VITE_REACT_APP_BACKEND_BASEURL}/api/v1/user";
+const API_END_POINT = `${import.meta.env.VITE_REACT_APP_BACKEND_BASEURL}/api/v1/user`;
 axios.defaults.withCredentials = true;
 
 type User = {
